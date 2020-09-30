e2767b3c6fdbb37591e8ca84a9fa613d1ca6b9d3
laxman <lxmn2010@gmail.com>
2020-10-01 03:19:28 +0800

    Updates

---
+++ precache-manifest.447add03e2c66598655fddc42cd2019d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fbcc14c670996742fd980075ddd2260",
+    "url": "/shoppingcart-sg/index.html"
+  },
+  {
+    "revision": "ac148203d8bf808c170d",
+    "url": "/shoppingcart-sg/static/css/main.5bd29f58.chunk.css"
+  },
+  {
+    "revision": "5e7817b743c821c87f04",
+    "url": "/shoppingcart-sg/static/js/2.b3c1f642.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingcart-sg/static/js/2.b3c1f642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac148203d8bf808c170d",
+    "url": "/shoppingcart-sg/static/js/main.2fc6f122.chunk.js"
+  },
+  {
+    "revision": "07b3335140a4e03aa262",
+    "url": "/shoppingcart-sg/static/js/runtime-main.0e6585d0.js"
+  }
+]);
